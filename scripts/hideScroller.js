998c2d007b71f7c3fa5735fd8cbac099423bd3f3
happy-cutman <happy.cutman@gmail.com>
2022-11-30 14:31:42 +0200

    EDIT | scroller script

--- scripts/hideScroller.js
+++ scripts/hideScroller.js
@@ -1,23 +1,20 @@
 let scroller = document.getElementById('scroller')
 
 const showOnScroll = () => {
-  let scrollPos = window.pageYOffset
 
-  if (scrollPos > 100) {
-    scroller.style.display = 'none'
-  } else {
-    scroller.style.display = 'flex'
-  } 
+  document.addEventListener('scroll', function () {
+    let scrollPos = window.pageYOffset
+
+    if (scrollPos > 100) {
+      scroller.style.display = 'none'
+    } else {
+      scroller.style.display = 'flex'
+    } 
+  })
+  
 }
 
-const showScroller = () => {
-  if (window.innerWidth <= 1024) {
-    document.addEventListener('scroll', showOnScroll)
-  } else {
-     document.removeEventListener('scroll')
-  }
-}
+showOnScroll()
 
-showScroller()
 
 
