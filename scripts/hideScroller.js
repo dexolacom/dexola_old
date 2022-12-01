01cad5c6cf474306de5bf47088d62ef93e5ef917
happy-cutman <happy.cutman@gmail.com>
2022-12-01 12:59:48 +0200

    ADD | mobile to all

--- scripts/hideScroller.js
+++ scripts/hideScroller.js
@@ -1,15 +1,27 @@
 let scroller = document.getElementById('scroller')
+let parentHeight = document.getElementById('scroller').parentNode.offsetHeight
+console.log(parentHeight)
+// 1250
 
 const showOnScroll = () => {
-  document.addEventListener('scroll', function () {
-    let scrollPos = window.pageYOffset
+  // if (window.innerWidth >= 1250) return scroller.style.display = 'none'
+  
 
-    if (scrollPos > 100) {
-      scroller.style.display = 'none'
-    } else {
-      scroller.style.display = 'flex'
-    } 
+  document.addEventListener('resize', function () {
+    console.log(1);
+    if (window.innerWidth >= 1250) return scroller.style.display = 'none'
   })
+
+
+  // document.addEventListener('scroll', function () {
+  //   let scrollPos = window.pageYOffset
+
+  //   if (scrollPos > 100) {
+  //     scroller.style.display = 'none'
+  //   } else {
+  //     scroller.style.display = 'flex'
+  //   } 
+  // })
 }
 
 showOnScroll()
