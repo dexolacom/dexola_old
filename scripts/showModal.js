770520747ca948d20989a0c7e20912f48e9131da
happy-cutman <happy.cutman@gmail.com>
2022-12-05 17:44:51 +0200

    ADD | sidebar slide

--- scripts/showModal.js
+++ scripts/showModal.js
@@ -1,8 +1,17 @@
 const scroller = document.getElementById('scroller')
 const sidebar = document.getElementById('sidebar')
 const header = document.getElementById('sidebar_header')
+const scrollHeight =document.getElementsByClassName('main_wrapper')[0].offsetHeight
+const mainWrapper = document.getElementsByClassName('main_wrapper')[0]
 
-scroller.addEventListener('click', () => {
-  sidebar.classList.add('.slide-top')
+scroller.addEventListener('click', () => {    
   header.style.display = 'flex'
-})
+  // const height = mainWrapper.getBoundingClientRect().top + document.documentElement.scrollTop
+  // console.log(scrollHeight);
+  // console.log(height);
+
+  
+
+  move('.sidebar_wrapper').y(0).duration('1s').then().set('position', 'fixed').y(0).end()
+  // sidebar.style.position = 'fixed'
+})  
